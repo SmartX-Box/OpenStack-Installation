13b204e1320d6d67411d91f9e93d188f80d44d94
SmartX-jshan <root@maas-node-1.maas>
2016-05-31 09:55:16 +0000

    add mysql, packages

---
+++ update_package.sh
@@ -0,0 +1,13 @@
+
+#This is installation for OpenStack Mitaka Release.
+
+
+#Add Repository and update
+apt-get install -y software-properties-common
+add-apt-repository -y cloud-archive:mitaka
+apt-get update && apt-get -y upgrade
+
+#reboot 
+
+
+
